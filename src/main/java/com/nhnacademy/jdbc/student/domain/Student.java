b581b1252c933957622d08404a7428434a2abffc
Taeyum <unimokw@naver.com>
2024-10-24 15:08:46 +0900

    JDBC

--- src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
+++ src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
@@ -5,9 +5,10 @@ import java.util.Objects;
 
 public class Student {
 
-    public enum GENDER{
-        M,F
+    public enum GENDER {
+        M, F
     }
+
     private final String id;
     private final String name;
     private final GENDER gender;
@@ -15,4 +16,28 @@ public class Student {
     private final LocalDateTime createdAt;
 
     //todo#0 필요한 method가 있다면 추가합니다.
+
+    public Student(String id, String name, GENDER gender, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public GENDER getGender() {
+        return gender;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StatementStudentRepository.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StatementStudentRepository.java
@@ -3,6 +3,7 @@ package com.nhnacademy.jdbc.student.repository.impl;
 import com.nhnacademy.jdbc.student.domain.Student;
 import com.nhnacademy.jdbc.student.repository.StudentRepository;
 import com.nhnacademy.jdbc.util.DbUtils;
+import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
@@ -17,28 +18,66 @@ public class StatementStudentRepository implements StudentRepository {
     @Override
     public int save(Student student){
         //todo#1 insert student
-
+        String sql = "INSERT INTO jdbc_students (id, name, gender, age) VALUES ('"
+                + student.getId() + "', '"
+                + student.getName() + "', '"
+                + student.getGender() + "', "
+                + student.getAge() + ")";
+        try(Connection connection = DbUtils.getConnection();
+            Statement statement = connection.createStatement()) {
+            return statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            log.error("Error while saving student: ",e);
+        }
         return 0;
     }
 
     @Override
     public Optional<Student> findById(String id){
         //todo#2 student 조회
-
+        String sql = "SELECT * FROM jdbc_students WHERE id = '" + id + "'";
+        try(Connection connection = DbUtils.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql)) {
+            if(resultSet.next()) {
+                String name = resultSet.getString("name");
+                Student.GENDER gender = Student.GENDER.valueOf(resultSet.getString("gender"));
+                int age = resultSet.getInt("age");
+                Student student = new Student(id, name, gender, age);
+                return Optional.of(student);
+            }
+        } catch (SQLException e) {
+            log.error("Error while finding student: ", e);
+        }
         return Optional.empty();
     }
 
     @Override
     public int update(Student student){
         //todo#3 student 수정, name <- 수정합니다.
-
+        String sql = "UPDATE jdbc_students SET name = '"
+                + student.getName() + "', gender = '"
+                + student.getGender().name() + "', age = "
+                + student.getAge() + " WHERE id = '" + student.getId() + "'";
+        try(Connection connection = DbUtils.getConnection();
+            Statement statement = connection.createStatement()) {
+            return statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            log.error("Error while updating student: ", e);
+        }
         return 0;
     }
 
     @Override
     public int deleteById(String id){
        //todo#4 student 삭제
-
+        String sql = "DELETE FROM jdbc_students WHERE id = '" + id +"'";
+        try(Connection connection = DbUtils.getConnection();
+            Statement statement = connection.createStatement()) {
+            return statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            log.error("Error while deleting student: ", e);
+        }
         return 0;
     }
 
