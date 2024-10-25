43241f804a47a797f782276a7cf893a50039bfe8
Taeyum <unimokw@naver.com>
2024-10-25 10:42:05 +0900

    JDBC

--- src/main/java/com/nhnacademy/jdbc/util/BasicConnectionPool.java
+++ src/main/java/com/nhnacademy/jdbc/util/BasicConnectionPool.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.jdbc.util;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.LinkedList;
@@ -27,29 +28,52 @@ public class BasicConnectionPool  {
 
     private void checkDriver(){
         //todo#1 driverClassName에 해당하는 class가 존재하는지 check합니다.
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         //존재하지 않는다면 RuntimeException 예외처리.
     }
 
     private void initialize(){
         //todo#2 maximumPoolSize만큼 Connection 객체를 생성해서 Connection Pool에 등록합니다.
+        try {
+            for(int i = 0 ; i < maximumPoolSize ; i++) {
+                Connection connection = DriverManager.getConnection(jdbcUrl,username,password);
+                connections.add(connection);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
-    public Connection getConnection() throws InterruptedException {
+    public synchronized Connection getConnection() throws InterruptedException {
         //todo#3 Connection Pool에 connection이 존재하면 반환하고 비어있다면 Connection Pool에 Connection이 존재할 때 까지 대기합니다.
-        return null;
+        while(connections.isEmpty()) {
+            wait();
+        }
+        return connections.poll();
     }
 
-    public void releaseConnection(Connection connection) {
+    public synchronized void releaseConnection(Connection connection) {
         //todo#4 작업을 완료한 Connection 객체를 Connection Pool에 반납 합니다.
+        connections.add(connection);
+        notify();
     }
 
-    public int getUsedConnectionSize(){
+    public synchronized int getUsedConnectionSize(){
         //todo#5 현재 사용중인 Connection 객체 수를 반환합니다.
-        return 0;
+        return maximumPoolSize - connections.size();
     }
 
     public void distory() throws SQLException {
         //todo#6 Connection Pool에 등록된 Connection을 close 합니다.
-
+        while(!connections.isEmpty()) {
+            Connection connection = connections.poll();
+            if(connection != null) {
+                connection.close();
+            }
+        }
     }
 }
